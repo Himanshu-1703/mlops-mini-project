4a54a3e44825a88df60dda6d2a3cd33c3f43a622
Himanshu Arora <129079320+Himanshu-1703@users.noreply.github.com>
2024-07-28 17:25:32 +0530

    dvc initialized

---
+++ src/data/data_ingestion.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from pathlib import Path
+from yaml import safe_load
+from src.logger import CustomLogger
+from typing import Tuple
+import logging
+
+# create the custom logger
+logger = CustomLogger(logger_name='data_ingestion')
+
+# create a stream handler
+console_handler = logging.StreamHandler()
+
+# add console handler to the logger
+logger.logger.addHandler(console_handler)
+
+# column to drop
+COLUMN_TO_DROP = 'tweet_id'
+
+# target column name
+TARGET = 'sentiment'
+
+# url for data
+URL = r'https://raw.githubusercontent.com/campusx-official/jupyter-masterclass/main/tweet_emotions.csv'
+
+
+
+
+def load_data_from_link(url: str) -> pd.DataFrame:
+    
+    try:
+        df = pd.read_csv(url)
+        logger.log_message('dataset downloaded from url')
+        
+        return df
+    except FileNotFoundError as e:
+        logger.log_message('Unable to download dataset')
+
+
+
+def do_data_splitting(dataframe: str) -> Tuple[pd.DataFrame,pd.DataFrame]:
+    try:
+        parameters = safe_load(open('params.yaml','r'))['data_ingestion']
+        logger.log_message('Parameters read successfully')
+    except FileNotFoundError as e:
+        logger.log_message('Parameters file missing')
+    
+    test_size = parameters['test_size']
+    random_state = parameters['random_state']
+    logger.log_message(f'Parameters : test_size={test_size}  random_state={random_state}')
+    
+    train_data, test_data = train_test_split(dataframe,test_size=test_size,random_state=random_state)
+    logger.log_message(f"""
+                       Data split into train data with shape {train_data.shape} and 
+                       test data with shape {test_data.shape}
+                       """)   
+     
+    return train_data, test_data
+
+def filter_classes(df: pd.DataFrame,target_column :str = TARGET) -> pd.DataFrame:
+    val_counts = df[target_column].value_counts(normalize=True)
+    classes_to_filter = val_counts[val_counts >= 0.2].index.tolist()
+    return df.loc[df[target_column].isin(classes_to_filter), :]
+    
+
+def save_the_data(dataframe: pd.DataFrame, data_path: Path) -> None:
+    try:
+        # save the data to path
+        dataframe.to_csv(data_path,index=False)
+        logger.log_message(f"DataFrame {data_path.name} saved successfuly")
+    except Exception as e:
+        logger.log_message(f'Dataframe not saved')
+        
+
+            
+def drop_column(dataframe: pd.DataFrame, column_name: str) -> pd.DataFrame:
+    return (
+        dataframe.drop(columns=[column_name])
+    )
+    
+    
+
+    
+def main():
+    # load the data from url
+    df = load_data_from_link(URL)
+    # drop the column from data
+    df_trans = drop_column(df,COLUMN_TO_DROP)
+    logger.log_message(f"Column name {COLUMN_TO_DROP} dropped from data")
+    
+    # split the data
+    train_df, test_df = do_data_splitting(df_trans)
+    
+    # filter the data based on frequent classes for train data
+    train_df = filter_classes(df=train_df)
+    logger.log_message(f"unique classes in training data = {train_df[TARGET].unique().tolist()}") 
+    # filter the data based on frequent classes for test data
+    test_df = filter_classes(df=test_df)
+    logger.log_message(f"unique classes in testing data = {test_df[TARGET].unique().tolist()}") 
+    
+    # save the train and test data to a directory
+    root_path = Path(__file__).parent.parent.parent
+    # data directory
+    data_path = root_path / "data" / "raw"
+    # make the raw data directory
+    data_path.mkdir(exist_ok=True,parents=True)
+    logger.log_message("Raw data folder created")
+    # save the train data
+    save_the_data(train_df,data_path / "train.csv")
+    # save the test data
+    save_the_data(test_df,data_path / "test.csv")
+
+
+    
+if __name__ == "__main__":
+    main()
+
---
+++ src/models/predict.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import logging
+import sys
+from src.logger import CustomLogger
+import joblib
+from pathlib import Path
+from typing import Tuple
+import json
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+
+TARGET = 'target'
+MODEL_NAME = 'rf.joblib'
+
+# custom logger for module
+logger = CustomLogger('predict')
+
+# create a stream handler
+console_handler = logging.StreamHandler()
+
+# add console handler to the logger
+logger.logger.addHandler(console_handler)
+
+# read the dataframe
+def read_data(data_path: Path) -> pd.DataFrame:
+    try:
+        df = pd.read_csv(data_path)
+    except FileNotFoundError:
+        logger.log_message(f'{data_path.name} does not exist')
+    else:
+        return df
+
+
+# make X and y
+def make_X_and_y(df: pd.DataFrame,target_column) -> Tuple[pd.DataFrame, pd.Series]:
+    X = df.drop(columns=target_column)
+    y = df[target_column]
+    logger.log_message('Data split into X and y')
+    return X, y
+
+        
+def do_predictions(model,X):
+    y_pred = model.predict(X)
+    return y_pred
+
+
+def load_model(model_path: Path):
+    try:
+        clf = joblib.load(model_path)
+    except FileNotFoundError as e:
+        logger.log_message("Model Path does not exist")
+    else:
+        logger.log_message("Model Loaded from path successfully")
+        return clf
+    
+
+        
+def calculate_metrics(data_name: str,metrics_obj: dict,y,y_pred):
+    accuracy = accuracy_score(y,y_pred)
+    precision, recall, f1_score, support = precision_recall_fscore_support(y,y_pred,
+                                                                           average=None,
+                                                                           zero_division=0.0)
+    
+    metrics_obj[data_name] = {'accuracy':accuracy,
+                              'precision':precision.tolist(),
+                              'recall':recall.tolist(),
+                              'f1_score':f1_score.tolist(),
+                              'support':support.tolist()}
+    
+    return metrics_obj
+
+
+def main():
+    root_path = Path(__file__).parent.parent.parent
+    data_path = root_path / "data" / "processed"    
+    model_load_path = root_path / "models" / "classifiers"
+    metrics_save_path = root_path / "reports" / "metrics.json"
+    metrics_dict = {}
+    
+    for ind in range(1,3):
+        filename = sys.argv[ind]
+        # read the data
+        df = read_data(data_path=data_path / filename)
+        # split into x and y
+        X,y = make_X_and_y(df=df,target_column=TARGET)
+        # load the model
+        clf = load_model(model_path=model_load_path / MODEL_NAME)
+        # get predictions
+        y_pred = do_predictions(model=clf,X=X)
+        # generate metrics
+        metrics_dict = calculate_metrics(data_name=(data_path / filename).name,
+                                         metrics_obj=metrics_dict,
+                                         y=y,y_pred=y_pred)
+        
+    # convert metrics dict to json
+    with open(metrics_save_path,"w") as f:
+        json.dump(metrics_dict,f,indent=4)
+    
+    logger.log_message("Metrics saved to save location")
+
+
+if __name__ == "__main__":
+    main()
+
+
